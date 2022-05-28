442b76d29b301c9546b5fc8254e5cb8c5e00c147
lullabyforkful <100482733+lullabyforkful@users.noreply.github.com>
2022-05-28 07:15:49 +0100

    Create debiansuite.sh

---
+++ debiansuite.sh
@@ -0,0 +1,58 @@
+sudo apt update && sudo apt upgrade
+apt install sddm xorg kde-plasma-desktop kde-window-manager
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list > /dev/null
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+sudo apt update && sudo apt install kwin-bismuth
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+exit
+
+# put a #in front if u dont want it
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.todoist.Todoist
+flatpak install flathub io.freetubeapp.FreeTube
+flatpak install flathub org.onlyoffice.desktopeditors
+flatpak install flathub com.github.eneshecan.WhatsAppForLinux
+flatpak install flathub network.loki.Session
+flatpak install flathub com.discordapp.Discord 
+
+sudo su
+sudo apt install plasma-nm terminator xfburn gparted caja caja-open-terminal caja-admin caja-gtkhash
+
+#for librewolf
+distro=$(if echo " bullseye focal impish jammy uma una " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+
+echo "deb [arch=amd64] http://deb.librewolf.net $distro main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+
+sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
+
+sudo apt update
+
+sudo apt install librewolf -y
+
+#for brave browser
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+
+#for signal
+
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+  
+  sudo apt update && sudo apt install signal-desktop
+
+#for session messenger idk commands that well will google another time
+
+#skip
+
