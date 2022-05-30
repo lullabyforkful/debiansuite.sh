6559d848501cec0391c91540edd31e5a880c5619
lullabyforkful <100482733+lullabyforkful@users.noreply.github.com>
2022-05-30 04:56:27 +0100

    Update debiansuite.sh

--- debiansuite.sh
+++ debiansuite.sh
@@ -1,5 +1,5 @@
 #i plan to hash the file after pulling it from github then compare that to a verified copy
-sudo apt update && sudo apt upgrade && apt install wget curl apt-transport-https aptitude terminator xfburn gparted dolphin gnupg &&
+sudo apt update && sudo apt upgrade && apt install wget apt-transport-https curl aptitude terminator xfburn gparted dolphin gnupg &&
 apt install sddm xorg kde-plasma-desktop kde-window-manager plasma-nm --no-install-recommends &&
 
 #flatpaks
@@ -14,40 +14,38 @@ flatpak install flathub io.freetubeapp.FreeTube &&
 flatpak install flathub org.onlyoffice.desktopeditors &&
 flatpak install flathub com.github.eneshecan.WhatsAppForLinux &&
 flatpak install flathub network.loki.Session &&
-flatpak install flathub com.discordapp.Discord  &&
+flatpak install flathub com.discordapp.Discord &&
 
 #tiling manager: bismuth for kde
-echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list > /dev/null
-wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-sudo apt update && sudo apt install kwin-bismuth
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list > /dev/null &&
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null &&
+sudo apt update && sudo apt install kwin-bismuth &&
 
 #for librewolf
-distro=$(if echo " bullseye focal impish jammy uma una " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
-echo "deb [arch=amd64] http://deb.librewolf.net $distro main" | sudo tee /etc/apt/sources.list.d/librewolf.list
-sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
-sudo apt update
-sudo apt install librewolf -y
+distro=$(if echo " bullseye focal impish jammy uma una " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi) &&
+echo "deb [arch=amd64] http://deb.librewolf.net $distro main" | sudo tee /etc/apt/sources.list.d/librewolf.list &&
+sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg &&
+sudo apt update &&
+sudo apt install librewolf -y &&
 
 #for brave browser
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg  &&
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list &&
+sudo apt update &&
+sudo apt install brave-browser &&
 
 #for signal
-
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-  sudo apt update && sudo apt install signal-desktop
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg &&
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null &&
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\ &&
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list &&
+  sudo apt update && sudo apt install signal-desktop &&
 
 #for session messenger idk commands that well will google another time
 
 #for proprietary wifi drivers only
 
-cd /etc/apt/sourceslist.d/
+cd /etc/apt/sourceslist.d/ &&
 
 echo "deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
@@ -56,7 +54,7 @@ deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-fr
 deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
 
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" | sudo tee /etc/apt/sources.list.d/proprietarydebian.list
+deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" | sudo tee /etc/apt/sources.list.d/proprietarydebian.list &&
 
 sudo apt update && sudo apt install firmware-iwlwifi && sudo rm /etc/apt/sources.list.d/proprietarydebian.list && sudo apt update
 #skip
